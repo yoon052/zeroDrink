69e91731112399fafb060de8edcafae7155e69a2
Yoon <Yoon@naver.com>
2023-05-16 20:58:09 +0900

    feat : modal 창, addItem 기능 추가
    modal 창 띄워서 음료와 이미지, 설명을 웹페이지에 추가하는 기능

---
+++ script/addItem.js
@@ -0,0 +1,26 @@
+function addItem(name, image, content) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+            <img src="${image}" alt="${name}" />
+            <span>${content}</span>
+        `;
+  document.querySelector("#container").appendChild(div);
+
+  const span = document.createElement("span");
+  span.innerHTML = name;
+  document.querySelector("#sidebar").appendChild(span);
+}
+
+function addItemHandle(e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const image = document.getElementById("image").value;
+  const content = document.getElementById("content").value;
+  addItem(name, image, content);
+  closeModal();
+}
+
+document
+  .getElementById("addItemForm")
+  .addEventListener("submit", addItemHandle);
